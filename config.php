143def4c084b2cf284ba9eeca5c26768e497cc14
Istiak Ahmed Khan <ahmedistiak9999@gmail.com>
2022-06-23 01:57:45 +0600

    first commit

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+$servername="127.0.0.1:3307";
+$username="root";
+$password="";
+$db="database2";
+$link = mysqli_connect($servername,$username,$password,$db) or die("ERROR: Could not connect. " . mysqli_connect_error());
+ 
+?>
---
+++ tempCodeRunnerFile.php
@@ -0,0 +1,2 @@
+<?php
+<div class="box">
