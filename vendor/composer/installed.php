18cce528aaf490a0b23c34e0d4bfe024b9a484d2
Istiak Ahmed Khan <istiakahmedjoy99@gmail.com>
2022-09-10 00:24:05 +0600

    composer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'istiak/healthio',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => 'e058571d248f38f350b3c0dffd7f35e331bc631d',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'istiak/healthio' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'e058571d248f38f350b3c0dffd7f35e331bc631d',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
